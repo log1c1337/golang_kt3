1f293606c63a10ad82e3436536801d3df24e5c3c
log1c1337 <152594790+log1c1337@users.noreply.github.com>
2024-11-20 16:44:45 +0300

    Добавлено  задание 1 по Go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	counter int
+	mu      sync.Mutex
+)
+
+func increment(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mu.Lock()
+	counter++
+	mu.Unlock()
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go increment(&wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Final Counter Value:", counter)
+}
